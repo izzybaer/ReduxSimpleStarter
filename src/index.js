bd015413ecf817c36b37b8b0d04e0ddb0c7e7e41
Izabella Baer <izabellabaer@gmail.com>
2017-11-13 19:03:25 -0800

    got videos appearing on the page with thumbnails

--- src/index.js
+++ src/index.js
@@ -2,24 +2,31 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search-bar';
+import VideoList from './components/video-list';
 const API_KEY = 'AIzaSyAF0fNqK1KWYj3m8r-mAdWWJX4RaVGZy38';
 
-
+// now a class based component (aware of state, data changing over time)
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
       videos: [],
     };
+// need to pass data from App to VideoList (App is parent of VideoList)
     YTSearch({key: API_KEY, term: 'goats'}, (videos) => {
-      this.setState{(videos: videos)};
+      this.setState({videos: videos});
     });
   }
+
   render(){
     return (
       <div>
         <SearchBar />
+        <VideoList
+          videos={this.state.videos}
+         />
       </div>
+      // passing props 'videos' to VideoList
     );
   }
 }
